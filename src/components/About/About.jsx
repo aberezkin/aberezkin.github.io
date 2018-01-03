467a4cb0f64c0ef27aea1906de6f18c7d914d3e4
Arkadii Berezkin <arkm4n@gmail.com>
2018-01-03 10:41:17 +1000

    Add about component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from '../Image';
+import profilepic from './img/profilepic.jpg';
+
+const About = () => (
+    <Image src={profilepic} />
+);
+
+export default About;
---
+++ src/components/Image/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Image = styled.img`
+    ${props => props.rounded && css`
+        border-radius: 5px;
+    `}
+`;
+
+export default Image;
