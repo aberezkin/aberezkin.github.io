ba794f4ed7690474340fb3afe26730e2248be5b1
Arkadii Berezkin <arkm4n@gmail.com>
2018-01-03 21:30:29 +1000

    Add astronaut emoji to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,9 @@ import Title from '../Title';
 const Header = () => (
   <header>
     <div className="text">
-      <Title large>Hi, I&apos;m Ark. I build </Title>
-      <Title large>landing pages</Title>
+      <Title large><i className="em em-male-astronaut" /></Title>
+      <Title large>Hi, I&apos;m Ark and I build </Title>
+      <Title large>landing pages.</Title>
     </div>
   </header>
 );
